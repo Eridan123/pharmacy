c0d0ddbc469aca7e08e135c018a68abef41e205e
Eridan123 <saryguloveridan@gmail.com>
2019-05-24 10:43:26 +0600

    medicine list and config for templates

--- src/main/java/com/app/pharmacy/apteka/config/WebSecurityConfig.java
+++ src/main/java/com/app/pharmacy/apteka/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -76,6 +77,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe().tokenRepository(this.persistentTokenRepository()) //
                 .tokenValiditySeconds(1 * 24 * 60 * 60); // 24h
 
+    }
+
+
+    @Override
+    public void configure(WebSecurity web){
+        web
+                .ignoring()
+                .antMatchers("/resources/**","/css/**","/font-awesome/**","/fonts/**","/images/**","/js/**");
     }
 
     @Bean
